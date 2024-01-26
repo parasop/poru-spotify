413844a3d28b159343fb8106d02f6a67907e64e1
Oxyde <157843247+OxydeDev1789@users.noreply.github.com>
2024-01-26 22:52:19 +0100

    Update Spotify.d.ts

--- dist/Spotify.d.ts
+++ dist/Spotify.d.ts
@@ -16,7 +16,7 @@ export interface SpotifyAccessTokenAPIResult {
     access_token?: string;
     expires_in: number;
 }
-export type loadType = "TRACK_LOADED" | "PLAYLIST_LOADED" | "SEARCH_RESULT" | "NO_MATCHES" | "LOAD_FAILED";
+export type loadType = "track" | "playlist" | "search" | "empty" | "error";
 export interface SpotifyFollower {
     href: string;
     total: number;
